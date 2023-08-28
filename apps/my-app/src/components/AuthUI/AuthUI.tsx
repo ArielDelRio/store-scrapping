fd55388d3ee54aade0b30068e18dce5e77097255
Ariel del Rio <arieldelrioviamonte@gmail.com>
2023-08-27 23:38:41 -0400

    Add domain env

--- apps/my-app/src/components/AuthUI/AuthUI.tsx
+++ apps/my-app/src/components/AuthUI/AuthUI.tsx
@@ -4,11 +4,12 @@ import { Card, CardBody } from "@nextui-org/react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 const AuthUI = () => {
   const router = useRouter();
+  const pathname = usePathname();
   const supabase = createClientComponentClient<Database>();
 
   useEffect(() => {
@@ -19,6 +20,7 @@ const AuthUI = () => {
     });
   }, [router, supabase.auth]);
 
+  console.log({ pathname });
   return (
     <main className="min-h-screen grid place-content-center">
       <Card className="w-[30vw] dark:bg-neutral-900 border border-neutral-200 dark:border-neutral-700">
@@ -30,7 +32,7 @@ const AuthUI = () => {
             }}
             theme="dark"
             providers={["google", "facebook"]}
-            redirectTo="http://localhost:3000/api/auth/callback"
+            redirectTo={`${process.env.NEXT_PUBLIC_DOMAIN}/api/auth/callback`}
           />
         </CardBody>
       </Card>
